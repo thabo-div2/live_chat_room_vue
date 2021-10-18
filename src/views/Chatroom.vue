9d9a7900025fabd15627ce4716cdf753e9e93ad5
Thabo Setsubi <thabosetsubi3@gmail.com>
2021-10-18 13:14:35 +0200

    update commit

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,13 +1,34 @@
 <template>
-	<div class="container"><Navbar /></div>
+	<div class="container">
+		<Navbar />
+		<ChatWindow />
+		<NewChatForm />
+	</div>
 </template>
 
 <script>
+import { watch } from "@vue/runtime-core";
 import Navbar from "../components/Navbar.vue";
+import ChatWindow from "../components/ChatWindow.vue";
+import NewChatForm from "../components/NewChatForm.vue";
+import getUser from "../composables/getUser";
+import { useRouter } from "vue-router";
 
 export default {
 	components: {
 		Navbar,
+		NewChatForm,
+		ChatWindow,
+	},
+	setup() {
+		const { user } = getUser();
+		const router = useRouter();
+
+		watch(user, () => {
+			if (!user.value) {
+				router.push({ name: "Welcome" });
+			}
+		});
 	},
 };
 </script>
